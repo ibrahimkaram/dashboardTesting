6d66f7fd34ff2a848a51e8df9fda756b0b1c76e2
Luigi <weegesantoro@gmail.com>
2023-01-09 22:33:32 -0500

    small push

--- pages/launchpad.vue
+++ pages/launchpad.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
-    <h1>Bro connect your wallet</h1>
+    <h1>Welcome to Launchpad</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "ConnectWallet"
+  name: "launchpad"
 }
-
-// Ibrahim code here
-
 </script>
 
 <style scoped>

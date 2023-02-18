7747259abc1f5b484481d602ad5182f4a2badb57
Luigi <weegesantoro@gmail.com>
2023-02-18 12:46:21 -0500

    adding new factories

--- assets/constants/factories.js
+++ assets/constants/factories.js
@@ -1,5 +1,6 @@
 export const factoryAddresses = {
     'goerli': '0xe031289459D003c183483BCe69a586fCff3e78A0',
-    'polygon': '0xda196CF836c18962d21588700EE94f077E6032bf',
-    'mainnet': '0x23E50E5322ca7c5E6dDd81730d1bb2B1bcD7Ea19'
+    'polygon': '0xe5a55C2059CD7473cadC129EDf75FBD1C2237b45',
+    'mainnet': '0xff8cCD2495840bCD2ca48d23B48F2c9e74c69786',
+    'mumbai': '0xe5a55C2059CD7473cadC129EDf75FBD1C2237b45'
 };
